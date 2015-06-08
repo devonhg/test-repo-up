2f64168f4884be9045c009e8e58baab798ba59d5
devonhg <devon.hugh.godfrey@gmail.com>
2015-06-08 12:47:37 -0400

    v0.1.4

--- test-repo-up.php
+++ test-repo-up.php
@@ -3,7 +3,7 @@
 Plugin Name: test-repo-up
 Plugin URI:  http://wordpress.org/extend/plugins/health-check/
 Description: Checks the health of your WordPress install
-Version:     0.1.3
+Version:     0.1.4
 Author:      The Health Check Team
 Author URI:  http://wordpress.org/extend/plugins/health-check/
 Text Domain: health-check
