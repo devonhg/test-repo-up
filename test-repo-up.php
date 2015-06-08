b3fd4ef7f0a6fac369bc88ca55c838d1124b6ff4
devonhg <devon.hugh.godfrey@gmail.com>
2015-06-08 12:32:02 -0400

    puppies

---
+++ test-repo-up.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: test-repo-up
+Plugin URI:  http://wordpress.org/extend/plugins/health-check/
+Description: Checks the health of your WordPress install
+Version:     0.1.2
+Author:      The Health Check Team
+Author URI:  http://wordpress.org/extend/plugins/health-check/
+Text Domain: health-check
+Domain Path: /lang
+GitHub Plugin URI: https://github.com/devonhg/test-repo-up
+GitHub Branch: master
+ */
